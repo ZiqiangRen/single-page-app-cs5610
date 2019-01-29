040b15cf9a417610457555412cf18c53b5b250c5
ziqiang <ziqiang@ccs.neu.edu>
2019-01-28 22:47:14 -0500

    basic framework

--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom';
 //import './index.css';
 //import App from './App';
 //import * as serviceWorker from './serviceWorker';
+import HelloWorld from './components/hello'
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import '../node_modules/font-awesome/css/font-awesome.min.css';
+import WhiteBoard from './components/WhiteBoard'
+import Stateless from './components/Stateless'
+
 
 //ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<h1>hello world</h1>, document.getElementById("root"));
+ReactDOM.render(
+	<div className="container-fluid">
+		<Stateless message="test my stateless"/>
+		<WhiteBoard/>
+	</div>,
+	document.getElementById("root")
+);
 	
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-//serviceWorker.unregister();
+
