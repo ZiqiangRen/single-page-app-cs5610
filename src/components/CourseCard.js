472195201078cc10e7ce01bf67058e94cfe69c7b
ziqiang <ziqiang@ccs.neu.edu>
2019-02-01 20:44:51 -0500

    update path

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {Link} from 'react-router-dom'
+import {Link} from '../../node_modules/react-router-dom'
 
 const CourseCard = ({course, deleteCourse, selectCourse}) =>
   <div className="card">
